5044d01675334c9e6a6edde2cf928514c9079cd7
Albert Chu <chu11@llnl.gov>
2005-07-05 22:44:07 +0000

    re-order flags

--- src/libs/include/cerebro.h
+++ src/libs/include/cerebro.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- *  $Id: cerebro.h,v 1.8 2005-06-28 22:14:12 achu Exp $
+ *  $Id: cerebro.h,v 1.9 2005-07-05 22:44:07 achu Exp $
 \*****************************************************************************/
 
 #ifndef _CEREBRO_H
@@ -39,8 +39,8 @@
  */
 
 #define CEREBRO_METRIC_FLAGS_UP_ONLY              0x00000001
-#define CEREBRO_METRIC_FLAGS_NONE_IF_NOEXIST      0x00000002
-#define CEREBRO_METRIC_FLAGS_NONE_IF_DOWN         0x00000004
+#define CEREBRO_METRIC_FLAGS_NONE_IF_DOWN         0x00000002
+#define CEREBRO_METRIC_FLAGS_NONE_IF_NOEXIST      0x00000004
 #define CEREBRO_METRIC_FLAGS_MASK                 0x00000007
 
 /* 
