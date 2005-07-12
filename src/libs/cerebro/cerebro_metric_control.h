2731224494148b0bb6dcc98581921320140ebd2b
Albert Chu <chu11@llnl.gov>
2005-07-12 19:57:47 +0000

    remove pointless header

--- src/libs/cerebro/cerebro_metric_control.h
+++
@@ -1,8 +0,0 @@
-/*****************************************************************************\
- *  $Id: cerebro_metric_control.h,v 1.1 2005-07-12 19:57:07 achu Exp $
-\*****************************************************************************/
-
-#ifndef _CEREBRO_METRIC_CONTROL_H
-#define _CEREBRO_METRIC_CONTROL_H
-
-#endif /* _CEREBRO_METRIC_CONTROL_H */
