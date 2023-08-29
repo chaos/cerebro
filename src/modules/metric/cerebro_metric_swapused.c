db345ec9fb139d2b438c06c4b31bb4e04d38d9d7
Albert Chu <chu11@llnl.gov>
2023-08-28 21:55:22 -0700

    Globally update cerebro website url

--- src/modules/metric/cerebro_metric_swapused.c
+++ src/modules/metric/cerebro_metric_swapused.c
@@ -9,7 +9,7 @@
  *
  *  This file is part of Cerebro, a collection of cluster monitoring
  *  tools and libraries.  For details, see
- *  <http://www.llnl.gov/linux/cerebro/>.
+ *  <https://github.com/chaos/cerebro>.
  *
  *  Cerebro is free software; you can redistribute it and/or modify it under
  *  the terms of the GNU General Public License as published by the Free
